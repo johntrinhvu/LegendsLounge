f665206562e464b4cb642e51efffefa68eff013e
John Vu <johntrinhvu@yahoo.com>
2023-06-30 19:51:47 -0700

    fix random scrollwheel on my sidebar, add post schema

---
+++ models/post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  // Other fields for your post schema
+});
+
+module.exports = mongoose.model('Post', postSchema);
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const Post = require('./models/post');
+const User = require('./models/user');
 // Always require and configure near the top
 require('dotenv').config();
 // Connect to the database
@@ -25,6 +27,26 @@ const port = process.env.PORT || 3001;
 
 // Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'));
+app.post('/api/posts', async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const user = req.user;
+
+    // create new post
+    const post = new Post({
+      title,
+      content,
+      author: user._id,
+    });
+
+    // save post to db
+    await post.save();
+
+    res.status(201).json({ message: 'Post created successfully', post });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while creating the post' });
+  }
+})
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX/API requests
